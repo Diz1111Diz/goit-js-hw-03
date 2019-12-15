b453a7425e0043a801cda4c42a214ca35e81526e
Diz111Diz <kavunv@gmail.com>
2019-12-16 00:50:29 +0200

    add hw-3

---
+++ js/task-2.js
@@ -0,0 +1,16 @@
+const countProps = function(obj) {
+  number = 0;
+  for (key in obj){
+      number ++         
+  }
+  return number;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
---
+++ js/task-6.js
@@ -0,0 +1,27 @@
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const calculateTotalPrice = function(products, productName) {
+//  const values = [];
+
+//   for (const product of products){
+
+//     if (key in product){
+
+//       values.push(product[key]);
+//     }
+//   }
+  
+// return values;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(calculateTotalPrice(products, 'Радар')); // 5200
+
+console.log(calculateTotalPrice(products, 'Дроид')); // 2800
